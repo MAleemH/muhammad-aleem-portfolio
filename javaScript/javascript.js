6712c81c8d115db33ca03b9d7a7d92794d4e9dc4
Raphael <okolonnaemeka2017@gmail.com>
2022-12-20 04:28:24 -0500

    Modify js file.

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -1,13 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const body = document.querySelector('body');
+const intro = document.querySelector('.introduction');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
   body.classList.toggle('active');
+  intro.classList.toggle('active');
 });
 document.querySelectorAll('.nav-options').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
   body.classList.remove('active');
+  intro.classList.remove('active');
 }));
+
+
