2e7937d8314454cb40fc8d7d0b7c9a4af28c09f9
Ouail-01 <ouaillaarabi@gmail.com>
2022-12-20 09:37:26 +0100

    Style the menu

--- javaScript/javascript.js
+++ javaScript/javascript.js
@@ -4,10 +4,10 @@ const body = document.querySelector('body');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-  body.classList.toggle('b_active');
+  body.classList.toggle('active');
 });
 document.querySelectorAll('.nav-options').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
-  body.classList.remove('b_active');
+  body.classList.remove('active');
 }));
