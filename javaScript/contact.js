6fc79fa5378a7a1920221b2db82671fe1e5d3978
Raphael <okolonnaemeka2017@gmail.com>
2022-12-22 04:50:51 -0500

    Update the submit button.

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -1,4 +1,4 @@
-const formButton = document.querySelector('#form-btn');
+const formButton = document.querySelector('#form-button');
 const text = document.getElementById('email').value;
 const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
