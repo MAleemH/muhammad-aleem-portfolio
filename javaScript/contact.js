e998dea952032d397aa22ff09355e25ed72c5e0c
Ouail-01 <ouaillaarabi@gmail.com>
2022-12-22 11:00:10 +0100

    Update the Js

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -4,11 +4,11 @@ const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 formButton.addEventListener('click', (a) => {
   if (!(regx.test(text))) {
-    document.getElementById('error-message').innerHTML = 'Please enter the valid email i.e., lowercase, abc@gmail.com';
-    document.getElementById('error-message').style.visibility = 'visible';
-    document.getElementById('error-message').style.color = 'white';
-    document.getElementById('error-message').style.textAlign = 'center';
-    document.getElementById('error-message').style.background = 'red';
+    document.getElementById('error-popup').innerHTML = 'Please enter the valid email i.e., lowercase, abc@gmail.com';
+    document.getElementById('error-popup').style.visibility = 'visible';
+    document.getElementById('error-popup').style.color = 'white';
+    document.getElementById('error-popup').style.textAlign = 'center';
+    document.getElementById('error-popup').style.background = 'red';
     a.preventDefault();
   }
 });
