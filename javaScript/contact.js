60da508932166512ca31ed632ad0b84bf814972a
Muhammad Aleem Hashmi <aleemhashmi4321@gmail.com>
2022-12-22 17:02:46 +0500

    error message issue fixed

--- javaScript/contact.js
+++ javaScript/contact.js
@@ -1,14 +1,18 @@
 const formButton = document.querySelector('#form-button');
-const text = document.getElementById('email').value;
-const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
-
-formButton.addEventListener('click', (a) => {
-  if (!(regx.test(text))) {
-    document.getElementById('error-popup').innerHTML = 'Please enter the valid email i.e., lowercase, abc@gmail.com';
-    document.getElementById('error-popup').style.visibility = 'visible';
-    document.getElementById('error-popup').style.color = 'white';
-    document.getElementById('error-popup').style.textAlign = 'center';
-    document.getElementById('error-popup').style.background = 'red';
-    a.preventDefault();
-  }
+function validate() {
+  const txt = document.getElementById('email').value;
+  const regx = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  document.forms[0].onsubmit = function (e) {
+    if (!(regx.test(txt))) {
+      document.getElementById('error-message').innerHTML = 'Please enter valid email address! i.e, lowercase, abc@gmail.com';
+      document.getElementById('error-message').style.visibility = 'visible';
+      document.getElementById('error-message').style.color = 'white';
+      document.getElementById('error-message').style.background = 'red';
+      document.getElementById('error-message').style.textAlign = 'center';
+      e.preventDefault();
+    }
+  };
+}
+formButton.addEventListener('click', () => {
+  validate();
 });
